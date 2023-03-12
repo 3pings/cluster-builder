ea02002d6498afbae25e6da90d3a6151e5eed2a4
Justin Barksdale <justin@spectrocloud.com>
2023-03-12 14:16:42 -0400

    Updated template

--- modules/edge/vault.tf
+++ modules/edge/vault.tf
@@ -1,42 +1,37 @@
+data "local_file" "pem_file" {
+  depends_on = [null_resource.write_pem]
+  filename = "${path.module}/pem_${local.cluster_id}.pub"
+}
 
 resource "local_file" "kubeconfig" {
   content  = spectrocloud_cluster_edge_native.this.kubeconfig
   filename = "kubeconfig_${local.cluster_id}"
 }
 
-
 # null provider reading a file
 resource "null_resource" "write_pem" {
-
   depends_on = [local_file.kubeconfig]
-
   triggers = {
     # change this if a force re-run is needed
     always_run = "${timestamp()}"
   }
-
   provisioner "local-exec" {
     command = "python3 ${path.module}/read_pem_from_oidc.py >> ${path.module}/pem_${local.cluster_id}.pub"
-
     environment = {
       KUBECONFIG = local_file.kubeconfig.filename
     }
   }
 }
 
-data "local_file" "pem_file" {
-  depends_on = [null_resource.write_pem]
-  filename = "${path.module}/pem_${local.cluster_id}.pub"
-}
 
-resource "vault_jwt_auth_backend" "example" {
+resource "vault_jwt_auth_backend" "this" {
   type = "jwt"
   jwt_validation_pubkeys = [data.local_file.pem_file.content]
   # path = "demo"
 }
 
-resource "vault_jwt_auth_backend_role" "example" {
-  backend                          = vault_jwt_auth_backend.example.path
+resource "vault_jwt_auth_backend_role" "this" {
+  backend                          = vault_jwt_auth_backend.this.path
   role_name                        = "devweb-app"
   bound_audiences = ["https://kubernetes.default.svc.cluster.local"]
   bound_subject = "system:serviceaccount:default:default"
@@ -45,13 +40,4 @@ resource "vault_jwt_auth_backend_role" "example" {
   role_type       = "jwt"
 
 }
-
-# resource "vault_kubernetes_auth_backend_config" "example" {
-#   backend                = vault_jwt_auth_backend.example.path
-#   # kubernetes_host        = local.cluster_host
-#   # kubernetes_ca_cert     = base64decode(local.cluster_ca)
-#   # token_reviewer_jwt     = kubernetes_secret.vault_sa.data.token
-#   # issuer                 = "https://kubernetes.default.svc.cluster.local"
-
-# }
 
